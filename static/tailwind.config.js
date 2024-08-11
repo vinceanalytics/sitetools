4f5f81fc090b19e83ba89856a5537e1cca391740
gernest <geofreyernest@live.com>
2024-08-11 16:17:11 +0300

    setup tailwind

---
+++ static/tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    typography: {
+      default: {
+        css: {
+          color: '#333',
+          pre: {
+            backgroundColor: null,
+            color: null
+          },
+          a: {
+            color: '#3182ce',
+            '&:hover': {
+              color: '#2c5282',
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
