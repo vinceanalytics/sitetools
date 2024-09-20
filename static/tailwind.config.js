c4a6f8d0d761a633d2ad312cce79c54d8ba35ba8
Geofrey Ernest <geofreyernest@live.com>
2024-09-20 08:41:25 +0300

    setup tailwind

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -1,31 +1,56 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "../data/templates/**/*.html",
-  ],
+  content: ["../data/templates/**/*.html"],
   theme: {
-    container: {
-      center: true,
-      padding: '1rem',
+    fontFamily: {
+      sans: ["Inter, sans-serif"],
+      mono: ["JetBrains Mono"],
+      source: ["Source Sans Pro"],
+      logo: ["Red Hat Text"],
     },
-    typography: {
-      default: {
-        css: {
-          color: '#333',
-          pre: {
-            backgroundColor: null,
-            color: null
-          },
-          a: {
-            color: '#3182ce',
-            '&:hover': {
-              color: '#2c5282',
-            },
-          },
+    extend: {
+      colors: {
+        blue: {
+          50: "#f5f7ff",
+          100: "#ecf0ff",
+          200: "#d8e0ff",
+          300: "#b2c1ff",
+          400: "#8ba2ff",
+          500: "#6583ff",
+          600: "#3e64ff",
+          700: "#2d4cdb",
+          800: "#1f37b7",
+          900: "#132593",
         },
+        gray: {
+          50: "#f8fafc",
+          100: "#f0f5f9",
+          200: "#e1e8f0",
+          300: "#cad5e0",
+          400: "#91a4b7",
+          500: "#61758a",
+          600: "#445668",
+          700: "#304254",
+          800: "#1c2a3a",
+          900: "#0d1829",
+        },
+        "brand-pink": "#e44c75",
+        elixir: "#554994",
       },
     },
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      maxsm: { min: "100px", max: "639px" },
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
-}
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "class",
+};
